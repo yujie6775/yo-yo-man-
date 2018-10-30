1807e3c302d15871cb2ab4f8d43e57596c05f17a
yujie6775 <44600014+yujie6775@users.noreply.github.com>
2018-10-30 16:49:30 +0800

    Add files via upload

---
+++ 1.lua
@@ -0,0 +1,13 @@
+
+-- Abstract: Hello World
+-- Version: 2.0
+-- Sample code is MIT licensed; see https://www.coronalabs.com/links/code/license
+---------------------------------------------------------------------------------------
+
+local background = display.newImage("world.jpg", display.contentCenterX, display.contentCenterY )
+
+local myText = display.newText("Hello,World!", display.contentCenterX, display.contentWidth / 4, native.systemFont, 40 )
+myText:setFillColor( 1,100/255, 100/255 )
+
+local myText = display.newText("Hello,World!", display.contentCenterX, display.contentWidth / 2, native.systemFont, 40 )
+myText:setFillColor( 1,100/255, 100/255 )
